a1095c2c0fa4a49effed5218376e0a406a8fea60
Felipe <felipe_11sul@hotmail.com>
2025-05-21 15:15:59 -0300

    Usa DATABASE_URL do ambiente para conexão com PostgreSQL

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configuração do banco de dados PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Fl%40mengo09@localhost:5432/usinas_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
