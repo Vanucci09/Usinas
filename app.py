c6f1f7f18016732a4b9d2e8fa1c66d19a55c35e4
Felipe <felipe_11sul@hotmail.com>
2025-05-22 13:57:23 -0300

    Remove criação automática das tabelas após setup inicial

--- app.py
+++ app.py
@@ -20,8 +20,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
 
 # Modelos
 class Usina(db.Model):
